da5365a1234bebb9c23915267331876a3d9168ea
Alex Kappen <imaninconsp1cuouskappa@gmail.com>
2020-07-10 22:09:47 -0400

    Built jekyll site

---
+++ _site/scripts/appearance.js
@@ -0,0 +1,34 @@
+
+/* Toggle Switch
+Code courtesy of Ananya Neogi
+Source: https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
+*/
+
+const toggleSwitch = document.querySelector('.switcheroo input[type="checkbox"]');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark'); //add this
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light'); //add this
+  }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+const currentTheme = localStorage.getItem('theme') || null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+
+
