c8ca3b3b50c1f8565f0733e29a9dd49078a7297c
Alex Kappen <imaninconsp1cuouskappa@gmail.com>
2020-09-20 00:01:09 -0400

    Messing around with js description. Will redesign it

--- assets/scripts/appearance.js
+++ assets/scripts/appearance.js
@@ -48,9 +48,45 @@ window.onclick = function(event) {
       overlayBlur.classList.remove("overlay-blur");
     }
   }
+  
+  if (event.target != ratingBtns[0] && 
+    event.target != ratingBtns[1] &&
+    event.target != ratingBtns[2] &&
+    event.target != ratingBtns[3] &&
+    event.target != ratingBtns[4]) {
+    for (i=0;i<ratingBtns.length;i++) {
+      ratingExplanation[i].classList.remove('show-rating-expl');
+    }
+  }
 }
 
 // Rating system dropdowns
 
-let ratingExplanation = document.querySelector('.rating-explanation');
-let 
+let ratingExplanation = document.querySelectorAll('.rating-explanation');
+let ratingBtns = document.querySelectorAll('.rating-btn');
+
+/** Easy Button */
+ratingBtns[0].onclick = function() {
+  ratingExplanation[0].classList.toggle('show-rating-expl');
+}
+
+/** Medium Button */
+ratingBtns[1].onclick = function() {
+  ratingExplanation[1].classList.toggle('show-rating-expl');
+}
+
+/** Intermediate Button */
+ratingBtns[2].onclick = function() {
+  ratingExplanation[2].classList.toggle('show-rating-expl');
+}
+
+/** Hard Button */
+ratingBtns[3].onclick = function() {
+  ratingExplanation[3].classList.toggle('show-rating-expl');
+}
+
+/** Advanced Button */
+ratingBtns[4].onclick = function() {
+  ratingExplanation[4].classList.toggle('show-rating-expl');
+}
+
